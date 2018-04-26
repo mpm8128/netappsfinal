2ba064e3a3ebeea858d4e89acd24b884eabfc93c
Chris Bridges <bridgesc@vt.edu>
2018-04-26 15:14:33 -0400

    Added aggregator functionality

--- aggregator.py
+++ aggregator.py
@@ -1,55 +1,88 @@
 #! /usr/bin/python3
 
+import sqlite3
+import feedparser
+import time
 import pika
 from rmq_params import rmq_params
 import pickle
-import time
+
+check_rate = 60 # Time (in seconds) between checks
 
 def send_message(headline, timestamp, topic):
-    to_send = pickle.dumps((headline,timestamp))
-    channel.basic_publish(exchange=rmq_params["exchange"], routing_key=topic,
-                                body=to_send)
-
+    print("Sending '%s' to %s" % (headline, topic))
+    to_send = pickle.dumps((headline,timestamp,topic))
+    time.sleep(0.1)
+    channel.basic_publish(exchange=rmq_params["exchange"], routing_key=topic, body=to_send)
+        
 def publish_headlines(new_headlines, topic):
     timestamp = time.time()
     for headline in new_headlines:
-        #timestamp = headline["timestamp"]
         send_message(headline, timestamp, topic)
 
-if __name__ == "__main__":
-    #parse configuration/arguments
-    #   file(s) to read topics/feeds from?
-    #   host/address and port of message broker
+def save_headline(headline, conn):
+    # Checking to see if the headline is already in the database
+    cur = conn.cursor()
+    check_sql = 'SELECT * FROM headlines WHERE headline=?'
+    cur.execute(check_sql, [headline])
+    if cur.fetchone() is not None:
+        return False
+    
+    # Inserting the headline into the database
+    cur = conn.cursor()
+    sql = 'INSERT INTO headlines(headline) VALUES(?)'
+    try:
+        cur.execute(sql, [headline])
+    except Exception as e:
+        print(e)
+        return False
+    return True
 
-    #setup rabbitmq stuff
+def get_new_headlines():
+    conn = sqlite3.connect('feeds.db')
+    
+    headlines = {}
+    
+    # Iterate through feeds in the database
+    print("Parsing news feeds")
+    for row in conn.execute("SELECT * FROM feeds ORDER BY category"):
+        feed_name = row[1]
+        feed_url = row[2]
+        category = row[3]
+        curr_headlines = []
+        try:
+            feed = feedparser.parse(feed_url)
+            for entry in feed["entries"]:
+                headline = entry["title"]
+                if len(headline) > 0 and save_headline("%s - %s" % (headline, feed_name), conn):
+                    print("New headline: %s" % headline)
+                    curr_headlines.append({"title" : headline, "source" : feed_name})
+            if category in headlines:
+                headlines[category].extend(curr_headlines)
+            else:
+                headlines[category] = curr_headlines
+        except Exception as e:
+            print(e)
+       
+    # Saving changes and closing sqlite connection
+    conn.commit()
+    conn.close()
+            
+    print("Parsed")
+    
+    return headlines
+        
+if __name__ == "__main__":
+    # Set Up RabbitMQ stuff
     credentials = pika.PlainCredentials(rmq_params["username"], rmq_params["password"])
-    parameters = pika.ConnectionParameters(virtual_host=rmq_params["vhost"], 
-                                                credentials=credentials)
+    parameters = pika.ConnectionParameters('172.29.51.245', 5672, virtual_host=rmq_params["vhost"], credentials=credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-
-    last_time = time.time()
-
-    #periodically check RSS feeds
-    #while(True)
-    #   for topic in topics:
-    #       new_headlines = []
-    #       #check RSS feeds
-    #       for feed in rss_feeds:
-    #           #get/parse data
-    #           #if timestamp > last_time:
-    #               new_headlines.append(feed_data)
-    #       #end for
-    #       #if new_headlines != []:
-    #           publish_headlines(new_headlines, topic)
-    #   #end for
-    #
-    #   #update our idea of what is recent
-    #   last_time = time.time()
-    #
-    #   time.sleep(60*1) #sleep 1 minute
-    #end while
-
-    #send_message("test 0", int(time.time()), "politics")
-
-    connection.close()
+    
+    # Checking loop
+    while True:
+        all_headlines = get_new_headlines()
+        for category in all_headlines:
+            curr_headlines = [headline["title"] for headline in all_headlines[category]]
+            publish_headlines(curr_headlines, category)
+        time.sleep(check_rate)
